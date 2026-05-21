2354b9c75506a78e47e2af080c47ca790c88d63a
Abuskw <abubakarmusa08132@gmail.com>
2026-05-21 21:42:04 +0845

    Fix CORS for production

--- server/server.js
+++ server/server.js
@@ -13,12 +13,10 @@ const PORT = process.env.PORT || 5000;
 // ========================================
 
 // Allow requests from your React/Vite frontend
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 // ============ UPLOAD LECTURE ============
 
 const multer = require('multer');
